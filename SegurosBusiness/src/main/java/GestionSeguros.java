811da903a34369fe1b8b4e7b2969892e0da463ac
Diego Herrán <diego.herran.esteras@gmail.com>
2026-03-02 11:54:54 +0100

    Gestion Seguro sin compilar

--- SegurosBusiness/src/main/java/GestionSeguros.java
+++ SegurosBusiness/src/main/java/GestionSeguros.java
@@ -1,4 +1,53 @@
+import java.util.List;
 
-public class GestionSeguros {
+/**
+ * Gestion Seguros
+ * Implementa las interfaces IGestionClientes e IGestionSeguros.
+ */
+public class GestionSeguros implements IGestionClientes, IGestionSeguros {
 
+    private IClientesDAO clientesDAO; 
+    private ISegurosDAO segurosDAO;
+
+    public GestionSeguros(IClientesDAO clientesDAO, ISegurosDAO segurosDAO) {
+        this.clientesDAO = clientesDAO;
+        this.segurosDAO = segurosDAO;
+    }
+
+    @Override
+    public Cliente nuevoCliente(Cliente c) {
+        // TODO: Implementar alta de cliente [cite: 2473]
+        return null;
+    }
+
+    @Override
+    public Cliente bajaCliente(String dni) {
+        // TODO: Implementar baja de cliente [cite: 2475]
+        return null;
+    }
+
+    @Override
+    public Cliente cliente(String dni) {
+        // TODO: Implementar consulta de cliente (Caso de uso Consulta Cliente) [cite: 2537]
+        return null;
+    }
+
+
+    @Override
+    public Seguro nuevoSeguro(Seguro s, String dni) {
+        // TODO: Implementar nuevo seguro asociado a un cliente [cite: 2502]
+        return null;
+    }
+
+    @Override
+    public Seguro bajaSeguro(String matricula, String dni) {
+        // TODO: Implementar baja de un seguro [cite: 2505]
+        return null;
+    }
+
+    @Override
+    public Seguro anhadeConductorAdicional(String matricula, String conductor) {
+        // TODO: Implementar añadir conductor adicional [cite: 2508]
+        return null;
+    }
 }
